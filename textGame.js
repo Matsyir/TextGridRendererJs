b6d83f96447f43d6ad213fce7ebe1275240964d9
Matsyir <4everpluto@gmail.com>
2019-05-08 20:01:25 -0400

    Changed design/refactored/optimized
    instead of using string templates it now just uses a 2d array in javascript and puts each point (which holds char, color, and bgColor) into a span on the html page.

--- textGame.js
+++ textGame.js
@@ -1,83 +1,120 @@
-class Point {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
+// Basic text game rendering engine in js
+"use strict";
+
+// Point as represented in the text game. Holds a char, charColor, and bgColor
+class TextGamePoint {
+    constructor(char, charColor, bgColor) {
+        this.char = char;
+        this.charColor = charColor;
+        this.bgColor = bgColor;
     }
 }
+// Main TextGame class to handle the game.
+class TextGame {
+    // Static constants related to the game. (Call without (): TextGame.ROWS)
+    static get UPDATE_DELAY() { return 5; }
+    static get ROWS() { return 8; }
+    static get COLS() { return 24; }
 
-class TextGamePoint {
-	constructor(pos, char, color) {
-        this.char = char;
-        this.color = color;
-    }
-
-    static generateArr(width, height, initChar=" ") {
+    // Generate an array of TextGamePoints based on the static size.
+    static generateArr(initChar=" ", initTextColor="#000000", initBgColor="#FFFFFF") {
         let arr = [];
-        for (let i = 0; i < width; i++) {
-            arr[i] = [];
-            for (let j = 0; j < height; j++) {
-                arr[i][j] = new TextGamePoint(new Point(i,j), initChar, "#000000");
+        for (let r = 0; r < this.ROWS; r++) {
+            arr[r] = [];
+            for (let c = 0; c < this.COLS; c++) {
+                arr[r][c] = new TextGamePoint(initChar, initTextColor, initBgColor);
             }
         }
         
         return arr;
     }
-}
 
-const UPDATE_DELAY = 5; // ms
-const ROWS = 8;
-const COLS = 24;
-
-let game = {};
-let canvas = {};
-canvas.update = function() {
-    let str = ""
-    for (let r = 0; r < ROWS; r++) {
-        for (let c = 0; c < COLS; c++) {
-            if (c == 0 && r != 0) {
-                str += "<br>"
-            }
-            str += `\$\{game.points[${r}][${c}].char\}`;
+    static getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
         }
-    }
-    delete this.str;
-    this.str = eval('`' + str + '`');
-}
-
-
-function init() {
-    let div = $('<div id="game">').css({"font-family": "monospace", "font-size": "24px", "padding": "0px"});
-    $("body").append(div);
-
-    game.points = TextGamePoint.generateArr(ROWS, COLS, "@");
-    canvas.update();
-}
-
-function update() {
-    // game update logic
-    for (let r = 0; r < ROWS; r++) {
-        for (let c = 0; c < COLS; c++) {
-            if (Math.random() < 0.1) {
-                game.points[r][c].char = Math.random().toString(36).substring(2, 3).toUpperCase();
-            }
-        }
+        return color;
     }
 
-    canvas.update(game.points)
-    setTimeout(update, UPDATE_DELAY)
-    
-    draw();
+    constructor(initChar=" ", initCharColor="#000000", initBgColor="#FFFFFF") {
+        this.points = TextGame.generateArr(initChar, initCharColor, initBgColor);
+        this.updateTimeout = null;
+    }
+
+    getPointContainer(r, c) {
+        return $(`#r${r}c${c}`);
+    }
+    getPointChar(r, c) {
+        return this.points[r][c].char;
+    }
+    getPointCharColor(r, c) {
+        return this.points[r][c].charColor;
+    }
+    getPointBgColor(r, c) {
+        return this.points[r][c].bgColor;
+    }    
+
+    setPoint(row, col, char=null, charColor=null, bgColor=null) {
+        let pointContainer = this.getPointContainer(row, col);
+        if (char != null) {
+            this.points[row][col].char = char;
+            pointContainer.text(char);
+        }
+        if (charColor != null) {
+            this.points[row][col].charColor = charColor;
+            pointContainer.css("color", charColor);
+        }
+        if (bgColor != null) {
+            this.points[row][col].bgColor = bgColor;
+            pointContainer.css("background", bgColor);
+        }
+    }
+
+    // initialization: create div filled with spans for each point.
+    init() {
+        let pointContainers = '<div id="game" style="font-family: monospace; font-size: 24px; padding: 0px">';
+        for (let r = 0; r < TextGame.ROWS; r++) {
+            for (let c = 0; c < TextGame.COLS; c++) {
+                pointContainers = pointContainers.concat(`<span id="r${r}c${c}" style="color: ${this.points[r][c].charColor}; background: ${this.points[r][c].bgColor};">${this.points[r][c].char}</span>`);
+            }
+            pointContainers = pointContainers.concat("<br>");
+        }
+
+        pointContainers = pointContainers.concat("</div>");
+
+        $("body").append(pointContainers);
+    }
+
+    update() {
+        // make sure update is called every UPDATE_DELAY, so queue the method for the future
+        // before the method executes. Ideally, update should be faster than UPDATE_DELAY.
+        this.updateTimeout = setTimeout(this.update.bind(this), TextGame.UPDATE_DELAY);
+        // randomize the chars and their colors
+        for (let r = 0; r < TextGame.ROWS; r++) {
+            for (let c = 0; c < TextGame.COLS; c++) {
+                let char = null;
+                let charColor = null;
+                let bgColor = null;
+                if (Math.random() < 0.1) { // only randomize some random chars not all of them
+                    char = Math.random().toString(36).substring(2, 3).toUpperCase();
+                }
+                if (Math.random() < 0.1) { // only randomize some random colors not all of them
+                    charColor = TextGame.getRandomColor();
+                }
+                if (Math.random() < 0.1) { // only randomize some random bgColors not all of them
+                    bgColor = TextGame.getRandomColor();
+                }
+                this.setPoint(r, c, char, charColor, bgColor);
+            }
+        }
+    }
 }
 
-function draw() {
-    // game draw logic
+let game = new TextGame("@", "#43EF00", "#000000");
 
-    $("#game").html(canvas.str)
-
-    setTimeout(draw, UPDATE_DELAY)
-}
-
-$(function() {
-    init();
-    update();
+$(function(){
+    game.init();
+    game.update();
 });
